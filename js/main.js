2e944b12e3d3dcb331eca0da8b1d3a72b3bed9e7
adviislam10 <71040123+adviislam10@users.noreply.github.com>
2020-10-09 15:20:19 -0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -3,26 +3,45 @@
 // Set up Canvas and Graphics Context
 let cnv = document.getElementById("myCanvas");
 let ctx = cnv.getContext("2d");
-cnv.width = 800;
-cnv.height = 600;
+initGraphics(800, 600);
 
 // Global Variables
-let xVals = [200];
-let yVals = [300];
+// Parallel Arrays to store Bubble Data
+let colorNames = ['red', 'green', 'blue', 'orange', 'magenta', 'purple']
+
+let xVals = [];
+let yVals = [];
+let rVals = [];
+let colorVals = [];
+for (let n = 0; n < 150; n++) {
+    xVals.push(Math.randomDec(0, cnv.width));
+    yVals.push(Math.randomDec(0, cnv.height));
+    rVals.push(Math.randomDec(10, 40));
+    colorVals.push(Math.randomElement(colorNames));
+}
+
 
 // Main Program Loop
 requestAnimationFrame(draw);
 
 function draw() {
-    // Logic
-    
 
     // Drawing
     ctx.clearRect(0, 0, cnv.width, cnv.height);
 
     // Draw Bubble
-    ctx.strokeStyle = 'green';
-    ctx.strokeCircle(xVals[0], yVals[0], 20)
+
+    for (let i = 0; i < xVals.length; i++) {
+        // Move Bubbles
+        xVals[i] += Math.randomDec(-5, 5);
+        yVals[i] += Math.randomDec(-5, 5);
+
+        // Draw Bubbles
+        ctx.strokeStyle = colorVals[i];
+        ctx.strokeCircle(xVals[i], yVals[i], rVals[i]);
+        
+    }
+    
 
     // Request another Animation Frame
     requestAnimationFrame(draw);
